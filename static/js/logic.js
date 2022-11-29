f05ca4716911d5082f8823aa96c84cd4d6ff8b85
ats_tandjoeng7 <n.atstandjoeng@gmail.com>
2022-11-29 12:12:09 -0700

    Update branch

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,8 +12,8 @@
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//  id: 'mapbox/streets-v11',
-  id: 'mapbox/dark-v11',
+//  id: 'mapbox/light-v11',
+  id: 'mapbox/light-v11',
   maxZoom: 18,
   tileSize: 512,
   zoomOffset: -1,
@@ -23,13 +23,45 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'streets' tile layer to the map.
 // Add a marker to the map for Los Angeles, CA
 // Create the map object with options
-var coords = Array([34.0522, -118.2437], L.latLng(38.575764, -121.478851), [33.835293, -117.914505]);
-var colors = Array('red', 'orange', 'brown', 'blue', 'pink', 'yellow');
+let coords = [[34.0522, -118.2437], L.latLng(38.575764, -121.478851), [33.835293, -117.914505]];
+let colors = Array('red', 'orange', 'brown', 'blue', 'pink', 'yellow');
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6214, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+let line1 = [
+  [37.6214, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
 // let zoomCoord = L.latLng(coords[0]);
-let zoomCoord = L.latLng(40.7, -94.5);
-let map = L.map('mapid').setView(zoomCoord, 4);
+// let zoomCoord = L.latLng(40.7, -94.5);
+// let map = L.map('mapid').setView(zoomCoord, 4);
+let zoomCoord = L.latLng(40.1975, -97.6664);
+let map = L.map('mapid').setView(zoomCoord, 5);
 
 streets.addTo(map);
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+//  color: "red"
+  color: "yellow"
+}).addTo(map);
+
+//let dashedLine = '<svg viewBox="0 0 30 12" xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="3" x2="30" y2="3" stroke="black" stroke-dasharray="4" /></svg>';
+let dashedLine = '<svg height="80" width="300"><g fill="none" stroke="black" stroke-width="4"><path stroke-dasharray="10,10" d="M5 40 l215 0" /></g></svg>';
+
+L.polyline(line1, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  // dashArray: 'dash, gap, dash, gap, ..'
+  dashArray: '15, 30, 45, 60'
+}).addTo(map);
 
 // get data from cities.js, an array containing each city's location, state, and population.
 let cityData = cities;
@@ -97,7 +129,7 @@ coords.forEach((coord, i) => {
 });
 
 // sequential for loop
-for (var i = 0; i < coords.length; i++) {
+for (let i = 0; i < coords.length; i++) {
   L.marker(coords[i]).addTo(map);
   L.circleMarker(coords[i], {
     radius: 300,
