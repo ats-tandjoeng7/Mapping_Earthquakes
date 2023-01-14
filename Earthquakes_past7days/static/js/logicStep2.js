9df643581be7a74fec3b441c1e4b0f4a9be1eb67
ats_tandjoeng7 <n.atstandjoeng@gmail.com>
2023-01-13 20:18:18 -0700

    Update html and js file

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,192 @@
+// UCB-VIRT-DATA-ANALYTICS-2022, Parto T.
+/* 
+  // Available mapbox api 
+  id: 'mapbox/streets-v12',
+  id: 'mapbox/outdoors-v12',
+  id: 'mapbox/light-v11',
+  id: 'mapbox/dark-v11',
+  id: 'mapbox/satellite-v9',
+  id: 'mapbox/satellite-streets-v12',
+  id: 'mapbox/navigation-day-v1',
+  id: 'mapbox/navigation-night-v1',
+*/
+// We create the tile layer that will be the default background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v12',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the outdoors view tile layer that will be an option for our map.
+let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/outdoors-v12',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the light view tile layer that will be an option for our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/light-v11',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/dark-v11',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the satellite view tile layer that will be an option for our map.
+let sat = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/satellite-v9',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the satellite-streets view tile layer that will be an option for our map.
+let satStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/satellite-streets-v12',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the navigation view tile layer that will be an option for our map.
+let navi = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/navigation-day-v1',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+// We create the navigation-night view tile layer that will be an option for our map.
+let naviNight = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/navigation-night-v1',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// Create a base layer that holds all maps.
+let baseMaps = {
+  Street: streets,
+  Outdoor: outdoors,
+  Light: light,
+  Dark: dark,
+  Satellite: sat,
+  SatelliteStreets: satStreets,
+  Navigation: navi,
+  NavigationNight: naviNight
+};
+
+// Create the map object with center, zoom level and default layer.
+const zoomCoord = L.latLng(39.5, -98.5);
+let map = L.map('mapid', {
+  center: zoomCoord,
+  zoom: 3,
+  layers: [streets]
+});
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+const githubName = "https://raw.githubusercontent.com/ats-tandjoeng7/";
+// Accessing the airport GeoJSON URL
+let airportData = githubName + "Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = githubName + "Mapping_Earthquakes/main/torontoRoutes.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = githubName + "Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+const quakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// create a style for the lines
+let myStyle = {
+  fillColor: '#ffffa1',
+  fillOpacity: 1,
+  weight: 2
+};
+
+// This function returns the style data for each of the earthquakes we plot on the map.
+// We pass the magnitude of the earthquake into a function to calculate the radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: '#ffae42',
+    color: '#000000',
+    radius: getRadius(),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+// Grabbing our GeoJSON data.
+d3.json(quakeData).then((data) => {
+  // This function returns the style data for each of the earthquakes we plot on the map.
+  // We pass the magnitude of the earthquake into a function to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: '#ffae42',
+      color: '#000000',
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+  // creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // turn each feature into a circleMarker on the map
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(`
+        <h2>Place: ${feature.properties.place}</h2><hr>
+        <h3>Magnitude: ${feature.properties.mag}</h3>
+        <h3>Depth: ${feature.geometry.coordinates[2]} km</h3>
+      `);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo
+  }).addTo(map);
+});
+
+/*
+// Grabbing our GeoJSON data.
+d3.json(quakeData).then((data) => {
+  // creating a geoJSON layer with the retrieved data
+  console.log(data);
+  L.geoJson(data, {
+//    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(`
+        <h2>Place: ${feature.properties.place}</h2><hr>
+        <h3>Magnitude: ${feature.properties.mag}</h3>
+        <h3>Depth: ${feature.geometry.coordinates[2]} km</h3>
+      `);
+    }
+  }).addTo(map);
+});
+*/
